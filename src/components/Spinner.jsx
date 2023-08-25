f4591fbe0a5c10bf43c3a3b793f19b0b8c45add6
Hopp-Murithi <hoppmurithi@gmail.com>
2023-08-25 15:48:11 +0300

    Spinner component jsx

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { MutatingDots } from "react-loader-spinner";
+
+const Spinner = () => {
+  return (
+    <div className="loader">
+      <MutatingDots
+        height="100"
+        width="100"
+        color="#0B66B1"
+        secondaryColor="#F4D32E"
+        radius="12.5"
+        ariaLabel="mutating-dots-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </div>
+  );
+};
+
+export default Spinner;
